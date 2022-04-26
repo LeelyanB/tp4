95b493202b2af1ecc45122881c1faf6b78a2bc9f
@benamane <benamane@et.esiea.fr>
2022-04-27 00:49:35 +0200

    noveau 2

--- bot.py
+++ bot.py
@@ -6,15 +6,15 @@ from multiprocessing.connection import Client
 from time import time
 import discord
 import datetime
-from dotenv import load_dotenv
 import os 
+from  dotenv import load_dotenv
 
 
 
 class MyBot(discord.Client,) :
     
     now =datetime.datetime.now()
-    now = now.strftime("%m/%d/%y, :%H/%M/%S")
+    now = now.strftime("%m/%d/%y, :%H:%M:%S")
     now = str(now)
 
     async def on_ready(self):
@@ -26,14 +26,14 @@ class MyBot(discord.Client,) :
     async def on_message(self,message):
        
        if message.content.startswith('hello'):
-          await message.channel.send('Hello guy'.format(message)) 
-       if  message.content.startswith('help'):
-           await message.channel.send('write hello ')
-
+          await message.channel.send('Hello dude'.format(message))  
        
        if message.author == self.user:
         with open("botlogs.txt", "a") as L :
-            L.write( self.now +"message"+ message.content)
+            L.write( self.now +"message "+ message.content)
+            
+       if  message.content.startswith('help'):
+          await message.channel.send('write hello ')    
           
         
 
@@ -42,82 +42,12 @@ class MyBot(discord.Client,) :
       await general_channel.send(content =f"Welcome!{ member.display_name}")
       
 
-class token(fichier):
-  
-  def readtoken():
-    with open("token.txt","r") as L:
-      token= L.readline().rstrip()
-
-    return token
-    """
-    async def log(self):
-      await client.wait_until_ready()
-      
-       
-      while not client.is_closed() :
-         try :
-           
-           
-          
-             with open("botlogs.txt", "a") as L :
-
-               if len(self.Connect) != 0:
-                  L.write("Time :{int(time.time())},connection:{Connect.pop([0])}\n")
-
-               if len(self.messageR) != 0:
-
-                 L.write("Time :{int(time.time())},message:{messageR.pop([0])}\n")
-
-               
-
-         except Exception as b:
-          
-            print(b)  
-     """
-          
 
 
 
 
-
-
-
-"""
-class Log() :
-
-  def __init__(self, fichier) :
-      self.fichier = "Mylog"
-   
-  def message (self,message) :
-    logger = logging.getlogger(self.fichier)
-    logger.setlevel(logging.DEBUG)
-    LB = logging.FileHandler(self.fichier)
-    formatter = logging.Formatter('%(levelname)-15s %(asctime)s %(message)s')
-
-    LB.setFormatter(formatter)
-    LB.setlevel(logging.Debug)
-    logger.info(message)
-    
-  def ErrorConnection(self) :
-
-    logger = logging.getlogger(self.fichier)
-    logger.setlevel(logging.DEBUG)
-    LB = logging.FileHandler(self.fichier)
-    formatter = logging.Formatter('%(levelname)-15s %(asctime)s %(message)s')
-    LB.setFormatter(formatter)
-    LB.setlevel(logging.Debug)
-    logger.addHandler(LB)
-    logger.error(" Connection error")
-   """
-
-
-
-
-
-
+load_dotenv(dotenv_path="config")
 client = MyBot()
-load_dotenv(dotenv_path = "config")
-token = token()
-client.run(token.readtoken)
+client.run(os.getenv("token"))
     
      
